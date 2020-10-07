8005753ba8ddfe891f29730e316a00c1714a4b4f
Rao Najmus Saqib <rsaqib@elm.com.sa>
2020-10-07 08:02:13 +0300

    Initial commit for open maps API auto

---
+++ README.md
@@ -0,0 +1,51 @@
+#servicejectmodel-#API Automation #TestNG
+prerequisite: Java is installed, IDE-eclipse/IntelliJ
+API http://api.openweathermap.org/data/3.0/stations
+
+conventions:- Above mention API is implemented in a microservices way, so model name and other naming conventions has been donein the same way
+
+clone the project and can be run from command line or editor- mvn clean test
+#Note:A Folder has been created as src/test/java/resourcesc.Ideally it should be src/test/resources.
+#Technologies/Tools used in building the framework
+- Eclipse - IDE
+- Maven - Build automation tool
+- Java - Programming language
+-RestAssurred
+- TestNG - Test Management library
+-Allure reports
+#Framework is being implemented using best practices as describe below
+POJO classes implementation of the services.--com.gk.openweather.station.model
+Services has been implemented--com.gk.openweather.station.service
+TestRunner- you can run the tests from com.gk.openweather.station.test==OpenweatherTestRunnerE2E.
+JSON data handler has been defined on the runner class and data can be fetched from json to tests
+JSON path usage to capture Attributes
+-----------------------------------------------------------------------------------------------
+Below are the few methods has been implemented-get()/post()/delete() apart from this there are six more tests
+validateUserShouldableToCreatStation()- to create a station on openmaps.
+validateUserIsAbleToDeleteStation()-To delete the latest station created
+public void ValidateUserIsAbleToRunTherequestandGetsSuccessstatusCode.
+
+Each method is being implemented as severity with allure feature 
+How To use the Framework-
+1)If you want to add any emdpints- Naviagte to Class EndPoints & define it there and define method in OpenMapsStationApihelper
+2)OR To write any new test case .Just call the helper class method in test for existing services or new.
+rite the method in the test class.
+After running the test allure results filder should generate
+-------------------------------
+#How to Generate Allure Reports
+===============================
+download https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/2.13.0/ for mac or windows
+allure --version
+Navigate to project directory & run allure serve allure-results
+
+In order to generate a report, we should install Allure command-line interpreter.
+
+Download the latest version as a zip archive from bintray.
+Then, click the Files tab and then download the .zip file for windows. For Linux, you can download .tgz file. For Mac use brew to install allure.
+Unpack the archive to allure-commandline directory.[C:\automationsoftware\allure-2.13.0\bin]
+
+Navigate to bin directory.
+Add allure to system PATH.
+
+NOTE:-After importing the project you may see the error- Cannot switch on a value of type String for source level below 1.7. Only convertible int values or enum variables are permitted- please select the quickfix.(jre 1.7 & above]
+-
---
+++ src/test/java/com/gk/openweather/station/test/OpenweatherTestRunnerE2E.java
@@ -0,0 +1,114 @@
+package com.gk.openweather.station.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.gk.openweather.station.service.OpenMapsStationAPIHelper;
+
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+
+public class OpenweatherTestRunnerE2E {
+	private OpenMapsStationAPIHelper openWeatherMapsAPI;
+
+	JSONObject stationWeatherMapApiParams;
+
+	static String latestCreatedStation;
+
+	@BeforeClass
+	public void init() throws IOException {
+		openWeatherMapsAPI = new OpenMapsStationAPIHelper();
+		InputStream details = null;
+
+		try {
+			String dataFileName = "configFile/WeatherMapStationsData.json";
+			details = getClass().getClassLoader().getResourceAsStream(dataFileName);
+			JSONTokener tokener = new JSONTokener(details);
+			stationWeatherMapApiParams = new JSONObject(tokener);
+		}
+
+		catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (details != null) {
+				details.close();
+			}
+		}
+	}
+
+	@Test(description = "Validate user is able to create station", priority = 10)
+	@Severity(SeverityLevel.BLOCKER)
+	public void validateUserShouldableToCreatStation() {
+		String appId = stationWeatherMapApiParams.getJSONObject("validAppId").getString("appid");
+		latestCreatedStation = openWeatherMapsAPI.createStation(appId).jsonPath().getString("ID");
+
+		System.out.println("stattion got created with the number =>>" + latestCreatedStation);
+
+	}
+
+	@Test(description = "validate user is able to delete the latest station id created", priority = 20)
+	@Severity(SeverityLevel.CRITICAL)
+	public void validateUserIsAbleToDeleteStation() {
+		String appid = stationWeatherMapApiParams.getJSONObject("validAppId").getString("appid");
+		int statusCode = openWeatherMapsAPI.deleteStation(appid, latestCreatedStation);
+		Assert.assertEquals(statusCode, 204, "user should get statuc code as 204");
+		System.out.println("print status code for delete" + statusCode);
+	}
+
+	@Test(description ="Validate user is able to run request upon providing a specific station id as a path param user should get status code 200", priority = 30)
+	@Severity(SeverityLevel.CRITICAL)
+	public void ValidateUserIsAbleToRunTherequestandGetsSuccessstatusCode() {
+
+		String appid = stationWeatherMapApiParams.getJSONObject("validAppId").getString("appid");
+		String mapstation = stationWeatherMapApiParams.getJSONObject("stationId").getString("mapStationId");
+		int stauscode = openWeatherMapsAPI.getSpecificStationStationResponse(appid, mapstation);
+
+		Assert.assertEquals(stauscode, HttpStatus.SC_OK, "status should display ok 200");
+
+	}
+
+	@Test(description = "Validate get station request is returning correct status code", priority = 40)
+	@Severity(SeverityLevel.NORMAL)
+	public void validateStatusCode() {
+		String appId = stationWeatherMapApiParams.getJSONObject("validAppId").getString("appid");
+		int statusCode = openWeatherMapsAPI.getStationsStatusCode(appId);
+		Assert.assertEquals(statusCode, HttpStatus.SC_OK, "Request executed successfully");
+	}
+
+	@Test(description = "Validate user is able to run request upon providing a specific station id", priority = 50)
+	@Severity(SeverityLevel.TRIVIAL)
+	public void validateSpecificStationByStationIdSuccess() {
+		String appid = stationWeatherMapApiParams.getJSONObject("validAppId").getString("appid");
+		String validStationId = stationWeatherMapApiParams.getJSONObject("stationId").getString("mapStationId");
+		int stauscode = openWeatherMapsAPI.getSpecificStationStationResponse(appid, validStationId);
+		Assert.assertEquals(stauscode, HttpStatus.SC_OK, "User should get message for not found");
+	}
+
+	@Test(description = "Validate user gets BAD Request response code upon providing wrong stationId", priority = 50)
+	@Severity(SeverityLevel.MINOR)
+	public void validateSpecificStationByStationIdWrongStatId() {
+		String appid = stationWeatherMapApiParams.getJSONObject("validAppId").getString("appid");
+		int stauscode = openWeatherMapsAPI.getSpecificStationStationResponse(appid, "5f78971bcca8ce0001ef6131x");
+
+		Assert.assertEquals(stauscode, HttpStatus.SC_BAD_REQUEST, "User gets Bad Request status code-400");
+
+	}
+
+	@Test(description = "Negative test--Validate upon passing a null value user shouldn't get success code", priority = 50)
+	@Severity(SeverityLevel.MINOR)
+	public void validateSpecificStationByStationIdWrongStatIdRespValid() {
+		String appid = stationWeatherMapApiParams.getJSONObject("validAppId").getString("appid");
+		int stauscode = openWeatherMapsAPI.getSpecificStationStationResponse(appid, latestCreatedStation);
+
+		Assert.assertEquals(stauscode, HttpStatus.SC_OK, "User gets Bad Request status code-400");
+
+	}
+}
