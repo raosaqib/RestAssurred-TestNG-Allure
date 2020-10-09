b7609668022ec69658462d270a6f5a70cef6ce41
raosaqib <rao.saqib@hotmail.com>
2020-10-09 10:36:59 +0300

    Update Endpoints.java

--- src/main/java/com/gk/openweather/station/constatnts/Endpoints.java
+++ src/main/java/com/gk/openweather/station/constatnts/Endpoints.java
@@ -8,15 +8,5 @@ public class Endpoints {
 	
 	public static final String DELETE_STATION  = "/{ID}";
 	
-	
-	public static final String CREATE_STATION ="";
-	//public static final String CREATE_STATION  = "";
-	
-/*	public static final String GET_ALL_PERSONS="/persons";
-	public static final String GET_SINGLE_PERSON="/persons/25";
-	public static final String CREATE_PERSON="/persons";
-	public static final String DELETE_PERSON="/persons/{id}";
-	public static final String UPDATE_PERSON="/persons/{id}";*/
-
-	
+	public static final String CREATE_STATION ="";	
 }
